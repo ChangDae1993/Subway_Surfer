9911574e599f9636a3d515d272f2c14b8cda947c
ChangDae1993 <jsj939639@gmail.com>
2024-12-04 15:46:08 +0900

    Tile_EDIT2_Light_ADD

--- Assets/02_Scripts/Objects/SpawnTiles.cs
+++ Assets/02_Scripts/Objects/SpawnTiles.cs
@@ -1,3 +1,6 @@
+using NUnit.Framework.Constraints;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnTiles : MonoBehaviour
@@ -20,6 +23,22 @@ public class SpawnTiles : MonoBehaviour
 
     public Vector3 generateVec;
 
+    [Space(10f)]
+    [Header("Light")]
+    public Material redOn;
+    public Material blueOn;
+
+    public GameObject LightLTr;
+    public MeshRenderer[] Llights;
+    Coroutine lightLBlink;
+
+    public GameObject LightRTr;
+    public MeshRenderer[] Rlights;
+
+
+    Coroutine lightRBlink;
+    private float blinkTime = 2f;
+
     //생성 될때, 혹은 ObjPool에서 나올 때
     private void OnEnable()
     {
@@ -34,7 +53,67 @@ public class SpawnTiles : MonoBehaviour
                 //Debug.Log(obstacle.transform.localPosition);
             }
         }
+
+        if(LightLTr != null)
+        {
+            if (lightLBlink != null)
+            {
+                StopCoroutine(lightLBlink);
+                lightLBlink = StartCoroutine(lightLBlinkCo());
+            }
+            else
+            {
+                lightLBlink = StartCoroutine(lightLBlinkCo());
+            }
+
+        }
+
+
+        if (LightRTr != null)
+        {
+            if (lightRBlink != null)
+            {
+                StopCoroutine(lightRBlink);
+                lightRBlink = StartCoroutine(lightRBlinkCo());
+            }
+            else
+            {
+                lightRBlink = StartCoroutine(lightRBlinkCo());
+            }
+        }
     }
+
+    IEnumerator lightLBlinkCo()
+    {
+        while(true)
+        {
+            for (int i = 0; i < Llights.Length; i++)
+            {
+                Llights[i].material = redOn;
+                yield return new WaitForSeconds(blinkTime);
+                Llights[i].material = blueOn;
+            }
+            yield return null;
+        }
+
+    }
+
+
+    IEnumerator lightRBlinkCo()
+    {
+        while(true)
+        {
+            for (int i = 0; i < Rlights.Length; i++)
+            {
+                Rlights[i].material = redOn;
+                yield return new WaitForSeconds(blinkTime);
+                Rlights[i].material = blueOn;
+            }
+            yield return null;
+        }
+
+    }
+
 
 
     //Destroy 될 때, 혹은 ObjPool로 돌아갈 때
@@ -42,7 +121,15 @@ public class SpawnTiles : MonoBehaviour
     {
         if (objSpawnPoint != null)
         {
+            if (lightLBlink != null)
+            {
+                StopCoroutine(lightLBlink);
+            }
 
+            if (lightRBlink != null)
+            {
+                StopCoroutine(lightRBlink);
+            }
         }
     }
 
@@ -50,6 +137,16 @@ public class SpawnTiles : MonoBehaviour
     void Start()
     {
         generateVec = new Vector3(0f, 0.3f, 0f);
+
+        if (LightLTr != null)
+        {
+            Llights = LightLTr.GetComponentsInChildren<MeshRenderer>();
+        }
+
+        if (LightRTr != null)
+        {
+            Rlights = LightRTr.GetComponentsInChildren<MeshRenderer>();
+        }
     }
 
     // Update is called once per frame
